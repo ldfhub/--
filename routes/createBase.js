de41ca6a36fe81b148e47c1fa85750d92ce971d8
lidengfeng <coderldf580@163.com>
2021-12-27 18:46:45 +0800

    ci: 添加sql测试

---
+++ routes/createBase.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+
+// /* GET home page. */
+// //创建数据库
+// router.get('/',(req,res) => {
+//     let sql = 'CREATE DATABASE nodemysql'
+//     db.query(sql,(err,result) => {
+//         if(err) throw err
+//         console.log(result)
+//         res.send('Database created ...')
+//     })
+// })
+
+
+// module.exports = router;
