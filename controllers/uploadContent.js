2bf56d25c70c034c444ec8b31186b9d079a53e3d
lidengfeng <coderldf580@163.com>
2021-12-31 18:34:27 +0800

    fix: 优化

--- controllers/uploadContent.js
+++ controllers/uploadContent.js
@@ -7,12 +7,19 @@ const upload = require('../models/uploadContent');
 // 上传文案中间件
 const uploadContent = async(req, res, next) => {
     res.set('Content-Type', 'application/json; charset=utf-8')
-    const { title, content, type } = rea.body;
+    const { title, content, type } = req.body;
     const addRes = await db.query(upload.AddUploadContent(title, content, type));
+    console.log(addRes,'0000')
     if (addRes) {
         res.render('succ', {
             data: JSON.stringify({
                 msg: '添加成功.'
+            })
+        })
+    } else {
+        res.render('fail', {
+            data: JSON.stringify({
+                msg: '添加失败.'
             })
         })
     }
--- routes/uploadContent.js
+++ routes/uploadContent.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { uploadContent } = require('../controllers/uploadContent')
 
-/* GET home page. */
 router.post('/content', uploadContent);
 
 module.exports = router;
