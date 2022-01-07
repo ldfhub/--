072c823f9a28aa38a5e580fa6ceea211a0da5b9a
lidengfeng <coderldf580@163.com>
2022-01-07 19:01:00 +0800

    测试token检验成功1

--- middlewares/authority.js
+++ middlewares/authority.js
@@ -3,17 +3,18 @@ const { Token } = require('../utils/tools')
 // 鉴权验证tokens
 const authority = (req, res, next) => {
     res.set('Content-Type', 'application/json; charset=utf-8')
-    // console.log(req.headers['authorization']);
-    // // console.log(req.header['Authorization'])
-    const tokenString = req.headers['authorization'].substring(7);
-    // console.log(tokenString)
+    const tokenString = req.headers['authorization']?.substring(7);
     const decode = Token.decrypt(tokenString)
     console.log(decode)
-    // res.render('fail',{
-    //     data: JSON.stringify({
-    //         msg: '未登陆'
-    //     })
-    // })
+    if (decode) {
+        next();
+    } else {
+        res.render('fail',{
+            data: JSON.stringify({
+                msg: 'token验证失败'
+            })
+        })
+    }
 }
 
 module.exports = {
--- routes/uploadContent.js
+++ routes/uploadContent.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { uploadContent } = require('../controllers/uploadContent');
 const { authority } = require('../middlewares/authority');
 
-router.post('/content', authority);
+router.post('/content', authority, uploadContent);
 
 module.exports = router;
