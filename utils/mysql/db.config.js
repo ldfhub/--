53d97bf328a969f0453b8bf86eeb16a511ec8ff2
lidengfeng <coderldf580@163.com>
2022-02-24 23:04:43 +0800

    ci: 修改数据库名

--- utils/mysql/db.config.js
+++ utils/mysql/db.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     host : 'localhost',
     port : 3306,//端口号
-    database : 'nodeSql',//数据库名
+    database : 'nodesql',//数据库名
     user : 'root',//数据库用户名
     password : 'admin12345'//数据库密码
 };
