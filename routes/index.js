f758e49c6696967d3ab159adc79922a22b06fe85
lidengfeng <coderldf580@163.com>
2022-03-09 17:37:10 +0800

    ci: 首页html测试

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '黎明花开' });
+  res.render('index1.html', { title: '黎明花开' });
 });
 
 module.exports = router;
