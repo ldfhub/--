576aea0d0b5a6624134adcfc02ec1025bae65bb2
lidengfeng <coderldf580@163.com>
2022-03-27 21:27:14 +0800

    ci: 测试index.html

--- app.js
+++ app.js
@@ -13,15 +13,22 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').__express);  
 // app.set('view engine', 'html');
-app.engine('html', require('ejs').__express);  
-app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("Cache-Control","no-store");//304
+//   next();
+// });
 
 app.use('/', indexRouter);
 app.use('/queryH', quertHomeRouter)
