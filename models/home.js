bca2bc139c676509afe4e0b7004cc2755eb77c47
lidengfeng <coderldf580@163.com>
2022-04-04 17:16:13 +0800

    ci: 优化接口

--- models/home.js
+++ models/home.js
@@ -6,10 +6,10 @@
 //     const query1 = `SELECT * FROM joke WHERE id >= ((SELECT MAX(id) FROM joke)-(SELECT MIN(id) FROM joke)) * RAND() + (SELECT MIN(id) FROM joke) LIMIT 1`
 //     return query1;
 // }
-const queryJoke = 'SELECT joke as content,id,collect,easyLike FROM joke WHERE id >= ((SELECT MAX(id) FROM joke)-(SELECT MIN(id) FROM joke)) * RAND() + (SELECT MIN(id) FROM joke) LIMIT 1';
-const queryLoveWords = 'SELECT ishan as content,setId as id,collect,easyLike FROM loveWords WHERE setId >= ((SELECT MAX(setId) FROM loveWords)-(SELECT MIN(setId) FROM loveWords)) * RAND() + (SELECT MIN(setId) FROM loveWords) LIMIT 1';
-const queryHotWords = 'SELECT content,setId as id,collect,easyLike FROM hotWords WHERE setId >= ((SELECT MAX(setId) FROM hotWords)-(SELECT MIN(setId) FROM hotWords)) * RAND() + (SELECT MIN(setId) FROM hotWords) LIMIT 1';
-const queryWisdom = 'SELECT zh as content,id,collect,easyLike FROM wisdom WHERE id >= ((SELECT MAX(id) FROM wisdom)-(SELECT MIN(id) FROM wisdom)) * RAND() + (SELECT MIN(id) FROM wisdom) LIMIT 1';
+const queryJoke = 'SELECT joke as content,id,collect,easyLike,en FROM joke WHERE id >= ((SELECT MAX(id) FROM joke)-(SELECT MIN(id) FROM joke)) * RAND() + (SELECT MIN(id) FROM joke) LIMIT 1';
+const queryLoveWords = 'SELECT ishan as content,setId as id,collect,easyLike,en FROM loveWords WHERE setId >= ((SELECT MAX(setId) FROM loveWords)-(SELECT MIN(setId) FROM loveWords)) * RAND() + (SELECT MIN(setId) FROM loveWords) LIMIT 1';
+const queryHotWords = 'SELECT content,setId as id,collect,easyLike,en FROM hotWords WHERE setId >= ((SELECT MAX(setId) FROM hotWords)-(SELECT MIN(setId) FROM hotWords)) * RAND() + (SELECT MIN(setId) FROM hotWords) LIMIT 1';
+const queryWisdom = 'SELECT zh as content,id,collect,easyLike,en FROM wisdom WHERE id >= ((SELECT MAX(id) FROM wisdom)-(SELECT MIN(id) FROM wisdom)) * RAND() + (SELECT MIN(id) FROM wisdom) LIMIT 1';
 const finalQuery = `(${queryJoke}) UNION ALL (${queryLoveWords}) UNION ALL (${queryHotWords}) UNION ALL (${queryWisdom})`
 module.exports = {
     finalQuery,
